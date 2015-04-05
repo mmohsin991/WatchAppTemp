fce675223abb1daf534ddbe10d2757fd6e6fd45f
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-04-05 14:54:28 +0500

    upload
    apple watch temp app

---
+++ WatchAppTemp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchAppTemp WatchKit Extension
+//
+//  Created by Mohsin on 04/04/2015.
+//  Copyright (c) 2015 PanaCloud. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
